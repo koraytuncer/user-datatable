8fa924706bb251de2f70ec49c9f32e066e7465fc
koraytuncer <ktuncerr@gmail.com>
2024-05-10 12:02:26 +0300

    Search Alanı Oluşturuldu - geliştirmeye devam edilecek

--- src/components/DataTable/DataTableHeader.jsx
+++ src/components/DataTable/DataTableHeader.jsx
@@ -1,24 +1,50 @@
-import {TableHead,TableRow,TableCell,Checkbox} from "@mui/material";
+import { TableHead, TableRow, TableCell, Checkbox, Typography,Box } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { CgSearch } from "react-icons/cg";
 import "../../styles/datatable/DataTableHeader.css";
 
 const DataTableHeader = () => {
-    const headerItems = [
-        { label: "Avatar", width: '50px' },
-        { label: "Name", width: '250px' },
-        { label: "Username", width: '250px' },
-        { label: "Email", width: '250px' },
-        { label: "Role", width: '150px' },
-        { label: "Action", width: 'auto' }
-      ];
+  const headerItems = [
+    { label: "Avatar", width: "50px" },
+    { label: "Name", width: "250px" },
+    { label: "Username", width: "250px" },
+    { label: "Email", width: "250px" },
+    { label: "Role", width: "150px" },
+    { label: "Action", width: "auto" },
+  ];
   return (
     <>
       <TableHead>
-        <TableRow>
-          <TableCell padding="checkbox">
+        <TableRow sx={{ backgroundColor: "#FFFFFF", height: 94 }}>
+          <TableCell
+            align="left"
+            colSpan={5}
+            sx={{ paddingLeft: 3, border: "none" }}
+          >
+            <CgSearch size={32} color="#82868C" />
+          </TableCell>
+          <TableCell
+            align="right"
+            colSpan={3}
+            sx={{ paddingRight: 4, border: "none" }}
+          >
+            <Box sx={{display:"flex",justifyContent:"end",alignItems:"center"}}>
+            <DeleteIcon className="actionIcon" />
+           <Typography className="actionText" sx={{fontSize:12,fontWeight:600, fontFamily:"Montserrat"}}>Delete</Typography>
+            </Box>
+          </TableCell>
+        </TableRow>
+        <TableRow sx={{ border: "none" }}>
+          <TableCell padding="checkbox" className="datatableCell">
             <Checkbox className="checkBox" />
           </TableCell>
           {headerItems.map(({ label, width }) => (
-            <TableCell key={label} align="left" sx={{ width: width }}>
+            <TableCell
+              key={label}
+              align="left"
+              sx={{ width: width }}
+              className="datatableCell"
+            >
               {label}
             </TableCell>
           ))}
