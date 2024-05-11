c2d0e2d3d10ea2b3e50779ed625f77a0ea79a421
koraytuncer <ktuncerr@gmail.com>
2024-05-11 11:23:12 +0300

    Alert için Stillendirme yapıldı

--- src/utils/alert.js
+++ src/utils/alert.js
@@ -1,17 +1,23 @@
 import Swal from 'sweetalert2';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import '../styles/alert.css';
 
 export const confirmDelete = (userId, onDeleteConfirm) => {
   Swal.fire({
     title: 'Are you sure?',
     text: "You won't be able to revert this!",
-    icon: 'warning',
+    icon: null,
     showCancelButton: true,
     confirmButtonColor: '#2940D3',
     cancelButtonColor: '#d33',
     confirmButtonText: 'Yes, delete it!',
-    cancelButtonText: 'No, cancel!'
+    cancelButtonText: 'No, cancel!',
+    customClass: {
+      popup: 'custom-swal-popup', // CSS sınıfı tanımı
+      title: 'custom-swal-title',
+      content: 'custom-swal-content'
+    }
   }).then((result) => {
     if (result.isConfirmed) {
       onDeleteConfirm(userId); // Kullanıcı silme işlemini gerçekleştir
