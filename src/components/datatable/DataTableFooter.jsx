4fb424f44495fa8fe967487233d2ab6a92f0d620
koraytuncer <ktuncerr@gmail.com>
2024-05-13 02:20:30 +0300

    Kullanıcı olmayınca pagination gizlendi

--- src/components/datatable/DataTableFooter.jsx
+++ src/components/datatable/DataTableFooter.jsx
@@ -1,13 +1,12 @@
 import { Box, Pagination } from "@mui/material";
 import PaginationItem from "@mui/material/PaginationItem";
 import { useUsersData } from "../../contexts/userdatatable/UsersDataContext";
-import { usePagination } from '../../contexts/userdatatable/PaginationContext';
+import { usePagination } from "../../contexts/userdatatable/PaginationContext";
 import "../../styles/datatable/DataTableFooter.css";
 
 const DataTableFooter = () => {
   const { users } = useUsersData();
-  const { rowsPerPage,setCurrentPage,currentPage } = usePagination();
-
+  const { rowsPerPage, setCurrentPage, currentPage } = usePagination();
 
   const count = Math.ceil(users.length / rowsPerPage);
 
@@ -16,39 +15,41 @@ const DataTableFooter = () => {
   };
 
   return (
-    <Box>
-      <Pagination
-         count={count}
-         page={currentPage}
-         onChange={handleChangePage}
-         color="primary"
-         shape="rounded"
-         sx={{
-          '.MuiPaginationItem-root': {
-            fontFamily: 'Montserrat !important',
-            fontSize: '14px !important',
-            fontWeight: '600',
-            color: '#E3E6EB', 
-            '&:hover': {
-              backgroundColor: '#cfcfcf',
-            }
-          },
-          '.MuiPaginationItem-page.Mui-selected': {
-            backgroundColor: 'var(--primary-color)',
-            color: '#FFF !important',
-          },
-          '.MuiPaginationItem-icon': {
-            color: '#82868C',
-            fontSize: '1.75rem !important',
-            '&:hover': {
-              backgroundColor: '#cfcfcf',
-            },
-          }
-        }}
-      />
-    </Box>
-
-    
+    <>
+      {count > 1 && (
+        <Box>
+          <Pagination
+            count={count}
+            page={currentPage}
+            onChange={handleChangePage}
+            color="primary"
+            shape="rounded"
+            sx={{
+              ".MuiPaginationItem-root": {
+                fontFamily: "Montserrat !important",
+                fontSize: "14px !important",
+                fontWeight: "600",
+                color: "#E3E6EB",
+                "&:hover": {
+                  backgroundColor: "#cfcfcf",
+                },
+              },
+              ".MuiPaginationItem-page.Mui-selected": {
+                backgroundColor: "var(--primary-color)",
+                color: "#FFF !important",
+              },
+              ".MuiPaginationItem-icon": {
+                color: "#82868C",
+                fontSize: "1.75rem !important",
+                "&:hover": {
+                  backgroundColor: "#cfcfcf",
+                },
+              },
+            }}
+          />
+        </Box>
+      )}
+    </>
   );
 };
 
